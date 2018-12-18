6d248063e77157127a90ee60fc53e571b56d4aca
aryan <aryanpatel2500@gmail.com>
2018-12-18 13:46:37 +0530

    First Commit

---
+++ polls/models.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+
+class Baseline(models.Model):
+	created = models.
+
+class Person(models.Model):
+
+    class Meta:
+        ve"
+        verbose_ns"
+
+    def __str__(self):
+        pass
+    )
+
+
+class Student(models.Model):
+
+    class Meta:
+        ve"
+        verbose_ns"
+
+    def __str__(self):
+        self.
+    ):
+	"""docstring for Student(models.Model):
+	
+	    class Meta:
+	        ve"
+	        verbose_ns"
+	
+	    def __str__(self):
+	        pass
+	    f __init__(self, arg):
+		super(Student(models.Model):
+		
+		    class Meta:
+		        ve"
+		        verbose_ns"
+		
+		    def __str__(self):
+		        pass
+		    .__init__()
+		self.arg = arg
+		
